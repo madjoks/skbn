aebdec2dacaf86d2c14b659bd114c8f0b97d9240
madjoks <madjoks@yandex.ru>
2016-11-11 02:10:09 +0300

    Update index.js
    task 2b

--- skbn1/src/index.js
+++ skbn1/src/index.js
@@ -4,20 +4,33 @@ import canonize from './canonize'
 
 
 
-const app = express();
+const app = express()
 
 app.get('/task2a', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
   const a = parseInt(req.query.a) || 0;
   const b = parseInt(req.query.b) || 0;
 
   res.send('' + (a + b));
 })
 
+app.get('/task2b', function (req, res) {
+  const fullname = req.query.fullname;
+  if (!fullname)
+    res.send('Invalid fullname');
+    
+  let fio = fullname.split(' ');
+  const fio_length = fio.length;
+  if (fio_length>3 || fio_length<1)
+    res.send('Invalid fullname');
+
+  let result = fio[fio_length-1];
+  fio.slice(0, fio_length-1).forEach((item)=>{
+    result += ' ' + item[0] + '.';
+  });
+  res.send(result);
+})
+
 app.get('/task2c', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
   const user_name = canonize(req.query.username || '');
   if (!user_name)
       res.send('Invalid username')
