ee03f5737fab034cc779040a7efb13db85ea3c87
madjoks <madjoks@yandex.ru>
2016-11-11 21:13:25 +0300

    Update canonize.js
    fix task2c

--- skbn1/src/canonize.js
+++ skbn1/src/canonize.js
@@ -1,5 +1,5 @@
 export default function canonize(url){
-    const re = new RegExp('(https?:)?(//)?([^/]*/)?@?([A-z]*)/?', 'i');
+    const re = new RegExp('(https?:)?(//)?([^/]*/)?@?([A-z\.]*)/?', 'i');
     const user_name = url.match(re);
     return user_name[4];
 };
