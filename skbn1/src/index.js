233e2e8ad169e9960601d44da0807953f84e83e2
ermakov <ermakov@eis.ru>
2016-11-25 22:26:42 +0300

    task2c

--- skbn1/src/index.js
+++ skbn1/src/index.js
@@ -54,6 +54,19 @@ app.get('/task2d', function (req, res) {
   return res.send('#' + color.toLowerCase());
 })
 
+app.get('/task2x', function (req, res) {
+  var i = req.query.i || 0;
+  i = parseInt(i);
+  var result = //тут алгоритм бога
+
+  var m = [1, 18, 243, 3240, 43254, 577368, 7706988, 102876480, 1373243544, 18330699168,
+    244686773808, 3266193870720, 43598688377184, 581975750199168, 7768485393179328,
+    103697388221736960, 1384201395738071424, 18476969736848122368, 246639261965462754048];
+  var k = m[i];
+
+  return res.send(''+ k);
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
